a0c488ee585f2947b57f3439f71166b51431613c
souhardya69 <samantasouhardya@hotmail.com>
2021-05-22 01:04:03 +0530

    mobile-client-init

--- utils/spawn.js
+++ utils/spawn.js
@@ -1,11 +1,16 @@
-const { spawn } = require('child_process');
-const childPython  = spawn('python',['Heart_Disease_Detection.py','3.0 55.0 0 0 0 0 0 0 0 0 0 0 0']);
-childPython.stdout.on('data',(data)=>{
-    console.log(data.toString());
-})
-childPython.stderr.on('data',(data)=>{
-    console.error(data.toString());
-})
-childPython.on('close',(code)=>{
-    console.log(code);
-})
+const { spawn } = require("child_process");
+var datum;
+const childPython = spawn("python", [
+  "Heart_Disease_Detection.py",
+  "3.0 55.0 0 0 0 0 0 0 0 0 0 0 0",
+]);
+childPython.stdout.on("data", (data)=>{
+    datum=data.toString()
+    console.log(datum)
+});
+childPython.stderr.on("data", (data) => {
+  console.error(data.toString());
+});
+childPython.on("close", (code) => {
+  console.log(code);
+});
